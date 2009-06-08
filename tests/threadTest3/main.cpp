0ac8853331de79a5f75dda68041022786f2b464b
Rafael Fernández López <ereslibre@ereslibre.es>
2009-06-08 12:05:54 +0200

    Add a test that will connect, disconnect and emit from two threads concurrently

---
+++ tests/threadTest3/main.cpp
@@ -0,0 +1,107 @@
+/*
+ * This file is part of the Ideal libraries
+ * Copyright (C) 2009 Rafael Fernández López <ereslibre@ereslibre.es>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <core/application.h>
+#include <core/concurrent.h>
+#include <core/mutex.h>
+#include <core/condvar.h>
+#include <core/timer.h>
+
+using namespace IdealCore;
+
+class AnObject
+    : public Object
+{
+public:
+    AnObject(Object *parent);
+
+    IDEAL_SIGNAL(aSignal);
+
+    void emitIt();
+    void slot();
+};
+
+AnObject::AnObject(Object *parent)
+    : Object(parent)
+    , IDEAL_SIGNAL_INIT(aSignal)
+{
+}
+
+void AnObject::emitIt()
+{
+    emit(aSignal);
+}
+
+void AnObject::slot()
+{
+    IDEAL_SDEBUG("*** Slot being called from thread " << pthread_self());
+}
+
+class OneClass
+    : public Concurrent
+{
+public:
+    OneClass(Object *parent);
+
+    AnObject *object;
+
+protected:
+    void run();
+};
+
+OneClass::OneClass(Object *parent)
+    : Concurrent(parent)
+{
+}
+
+void OneClass::run()
+{
+    connect(object->aSignal, object, &AnObject::slot);
+    connect(object->aSignal, object, &AnObject::slot);
+    connect(object->aSignal, object, &AnObject::slot);
+    connect(object->aSignal, object, &AnObject::slot);
+    connect(object->aSignal, object, &AnObject::slot);
+    disconnect(object->aSignal, object, &AnObject::slot);
+    disconnect(object->aSignal, object, &AnObject::slot);
+    disconnect(object->aSignal, object, &AnObject::slot);
+    disconnect(object->aSignal, object, &AnObject::slot);
+    object->emitIt();
+    object->emitIt();
+    object->emitIt();
+    object->emitIt();
+    object->emitIt();
+}
+
+int main(int argc, char **argv)
+{
+    Application app(argc, argv);
+
+    AnObject *anObject = new AnObject(&app);
+    OneClass oneClass(&app);
+    oneClass.object = anObject;
+    OneClass otherClass(&app);
+    otherClass.object = anObject;
+    oneClass.exec();
+    otherClass.exec();
+
+    Timer::callAfter(1000, &app, &Application::quit);
+
+    return app.exec();
+}
