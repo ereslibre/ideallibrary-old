5a10495dbba4a29e8efd389b71d2cab93cb3c1fe
Rafael Fernández López <ereslibre@ereslibre.es>
2009-06-21 02:32:07 +0200

    Remove this example as it is. We are moving forward, and this is pretty outdated

--- core/extension_loader.h
+++ core/extension_loader.h
@@ -39,65 +39,6 @@ namespace IdealCore {
 class IDEAL_EXPORT ExtensionLoader
 {
 public:
-    /**
-      * Tries to load a module with name @p name.
-      *
-      * When you are given the result, you can ask the Module class for the extensions it
-      * contains. You can load extensions using ExtensionLoader::loadExtension method.
-      *
-      * @return The module with name @p name if the loading of the module was successful. 0
-      *         otherwise.
-      *
-      * @note You should never delete the returned content. The Ideal Library will take care of it.
-      *       The way it works is: when you call to loadExtension() a reference counter goes up 1 on
-      *       the module that contains that extension. When that extension is deleted the reference
-      *       counter goes down 1. If this counter hits 0, the module is automatically deleted and
-      *       removed from memory.
-      *
-      * @note For the automatic module freeing it is necessary that Application::exec() has been
-      *       called (there is an event loop running).
-      *
-      * @warning If you call to loadModule() but never call to loadExtension() using that module,
-      *          it will never be autodeleted. You are responsible of deleting it as if it were a
-      *          regular pointer. You can use the method Module::isUnused() to find out if you can
-      *          directly delete it.
-      *
-      * @see Module
-      */
-    static Module *loadModule(const String &name, Object *object)
-    {
-        return Private::loadModule(name, object);
-    }
-
-    /**
-      * Loads an extension that is at entry point @p entryPoint in @p module.
-      *
-      * When you load an extension (and it succeeds), the module reference counter goes up 1.
-      *
-      * @return On success, the extension is loaded and returned, reparented to @p parent. 0 is
-      *         returned otherwise.
-      *
-      * @note The ownership of the pointer is transferred to the user. You are responsible of
-      *       deleting the returned pointer when you are done with it.
-      *
-      * @note The returned object is automatically reparented before being returned, so if you
-      *       delete the parent object (and it did not have the isDeleteChildrenRecursively
-      *       disabled), this object will also become deleted.
-      */
-    template <typename T>
-    static T *loadExtension(Module *module, const String &entryPoint, Object *parent)
-    {
-        if (!parent) {
-            IDEAL_DEBUG_WARNING("the extension parent cannot be NULL");
-            return 0;
-        }
-        Extension *extension = Private::loadExtension(module, entryPoint);
-        if (extension) {
-            extension->reparent(parent);
-        }
-        return static_cast<T*>(extension);
-    }
-
     class ExtensionLoadDecider
     {
     public:
