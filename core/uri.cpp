42e35318c8a0a724c394901803c8887db4f01dce
Rafael Fernández López <ereslibre@ereslibre.es>
2009-06-25 15:21:48 +0200

    No way to reach those values, we are doing a bit and operation with 0xF (maximum value is 15)

--- core/uri.cpp
+++ core/uri.cpp
@@ -60,8 +60,7 @@ std::string unreserved_Ideal = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 std::string reserved_Ideal = "!*'();:@&=+$,/?%#[]";
 
 char hex_Ideal[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                     'A', 'B', 'C', 'D', 'E', 'F',
-                     'a', 'b', 'c', 'd', 'e', 'f' };
+                     'A', 'B', 'C', 'D', 'E', 'F' };
 
 String Uri::Private::getHex(char ch) const
 {
